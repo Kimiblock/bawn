a07d7b5874251c36fed625b39114e2336cdeaa5d
Kimiblock Moe <kimiblock@icloud.com>
2026-06-22 21:10:38 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+	let args = std::env::args_os();
+	let args_len = args.len();
+	let trailing_s: char = 's';
+	if args_len <= 1 {
+		help();
+		return std::process::ExitCode::from(1);
+	}
+	println!("Bawn started with {} argument{}", args_len, trailing_s);
+
+	ExitCode::SUCCESS
+}
+
+fn help() {
+	println!("This is bawn, a transient profile generator for the Portable sandbox");
+	println!("Usage:");
+	println!("	bawn <sandbox name>");
+	println!("	Note that <option> means required, [option] means optional");
 }
---
+++ src/types.rs
@@ -0,0 +1,4 @@
+pub struct CmdOptions {
+	SandboxName:		String
+}
+
