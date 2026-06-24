444cb9c554fc05360e98269b63122660096c9010
Kimiblock Moe <kimiblock@icloud.com>
2026-06-24 11:22:00 +0800

    create runtime directory

--- src/start.rs
+++ src/start.rs
@@ -1,4 +1,5 @@
 use crate::types;
+//use std::fs::File;
 //use std::result::Result;
 
 type Result<T> = std::result::Result<T, StartError>;
@@ -22,6 +23,54 @@ pub fn start_portable(config: &types::PortableConfig) -> Result<Option<String>> 
 			return Err(StartError(e.to_string()))
 		},
 	};
+
+	let xdg_dir = xdg::BaseDirectories::with_prefix("bawn");
+
+	match xdg_dir.has_runtime_directory() {
+		true => {}
+		false => {
+			return Err(
+				StartError("Could not find runtime directory".to_string())
+			);
+		}
+	}
+
+	let runtime_file = xdg_dir.get_runtime_directory();
+	match runtime_file {
+		Ok(_rt) => {}
+		Err(e) => {
+			return Err(StartError(e.to_string()));
+		}
+	}
+
+	let mut config_path: std::path::PathBuf = [
+		&runtime_file.unwrap(),
+	]
+		.iter()
+		.collect();
+	config_path.push("gen");
+
+	let exists = std::fs::exists(&config_path);
+	match exists {
+		Ok(true) => {}
+		Ok(false) => {
+			let result = std::fs::create_dir_all(&config_path);
+			match result {
+				Ok(()) => {}
+				Err(e) => {
+					return Err(StartError(e.to_string()))
+				}
+			}
+			println!("Created Runtime Directory for bawn");
+		}
+		Err(e) => {
+			return Err(StartError(e.to_string()))
+		}
+	}
+
+
+
+
 
 	Ok(Some(String::new()))
 }
