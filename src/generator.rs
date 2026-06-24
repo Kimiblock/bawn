161277a32a83bcf67316b77b14a5cd507fdec358
Kimiblock Moe <kimiblock@icloud.com>
2026-06-24 16:26:01 +0800

    fix spacing in friendlyName

--- src/generator.rs
+++ src/generator.rs
@@ -5,7 +5,7 @@ impl crate::types::PortableConfig {
 	pub fn new (sandbox_name: &String) -> Self {
 		let mut id = String::from("org.kraftland.portable.");
 		id.push_str(&sandbox_name.to_string());
-		let mut name = String::from("Bawn of Portable: ");
+		let mut name = String::from("Bawn-transient-");
 		name.push_str(sandbox_name);
 		types::PortableConfig {
 			metadata: types::PortableMetadata {
